9cb211ce13cbe9b813892332f4d45dd345bfd9ae
Slava <viatcheslav.bugayov@gmail.com>
2019-05-16 21:42:44 -0500

    Handling spaces correctly.

--- convert.sh
+++ convert.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-WORKDIR=~/RIP
+# WORKDIR=~/RIP
+WORKDIR=/data/bugaev/CompFS/ace/samba/Shares/Public/INCOMING/RAW_VIDEO
 EXE=/nix/store/aivkar15gvwmb943ijnnzfmd2isjd4j5-handbrake-1.0.7/bin/HandBrakeCLI 
 
 ts_p() {
@@ -22,13 +23,14 @@ ts_p() {
 }
 
 convert() {
-	src_nodir="$1"
-        src_fullpath="$WORKDIR/$src_nodir"
-        src_base=$(basename "$src_nodir" .ts)
-	dst_fullpath="$WORKDIR/$src_base.m4v"
-	dstlog_fullpath="$dst_fullpath.log"
-	# $EXE -i $src_fullpath -o $dst_fullpath -e x264 -q 20 --audio-lang-list eng --subtitle-lang-list eng --all-subtitles   --subtitle-burned=none 2>&1 | tee $dstlog_fullpath
-	echo "Converting $1 ..."
+    src_nodir="$1"
+    echo "In convert: got src_nodir=$src_nodir<--"
+    src_fullpath="$WORKDIR/$src_nodir"
+    src_base=$(basename "$src_nodir" .ts)
+    dst_fullpath="$WORKDIR/$src_base.m4v"
+    dstlog_fullpath="$dst_fullpath.log"
+    nice $EXE -i "$src_fullpath" -o "$dst_fullpath" -e x264 -q 20 --audio-lang-list eng --subtitle-lang-list eng --all-subtitles   --subtitle-burned=none 2>&1 | tee "$dstlog_fullpath"
+    echo "Converting $1 ..."
 }
 
 inotifywait -m -e close_write -e moved_to --format %f ~/RIP |
